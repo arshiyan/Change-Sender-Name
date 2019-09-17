4bd0c23d92ae1eec375de92371fb92c50ccfb339
javadarshiyan <dr.bloger@gmail.com>
2019-09-17 15:02:23 +0430

    bug fix

--- index.php
+++ index.php
@@ -65,7 +65,7 @@ function mailsender_text() {
 function mailsender_menu() {
 	add_menu_page(__('Mail Sender Options', 'mailsender'), __('Mail Sender', 'mailsender'), 'manage_options', 'mailsender', 'mailsender_output', 'dashicons-email');
 
-add_submenu_page( 'mailsender', __('Test Mail', 'mailsender'), __('Test Mail', 'mailsender'), 'manage_options', 'wp-test-email', 'wp_test_email' );
+add_submenu_page( 'mailsender', __('Test Mail', 'mailsender'), __('Test Mail', 'mailsender'), 'manage_options', 'mailsender', 'wp_test_email' );
 
 }
 add_action('admin_menu', 'mailsender_menu');
@@ -88,7 +88,7 @@ function wp_test_email()
 {	
 ?>
 <div class="wrap">
-	<h1><?php _e( 'Test Mail', 'wp-test-email' ); ?></h1>
+	<h1><?php _e( 'Test Mail', 'mailsender' ); ?></h1>
 	<form method="post">		
 		<?php
 		if(isset($_POST['mail_to']))
@@ -100,14 +100,14 @@ function wp_test_email()
 				{
 					$to=sanitize_email($_POST['mail_to']);
 					$subject=sanitize_text_field($_POST['mail_subject']);
-					$body=" This is the test mail from ".get_bloginfo('name');
+					$body="hi,  This is a test email from ".get_bloginfo('name') ." that sent you";
 					$headers = array('Content-Type: text/html; charset=UTF-8'); 
 					$test_email=wp_mail( $to, $subject, $body );
 					if($test_email)
 					{
 						?>
 						<div class="notice notice-success is-dismissible">
-							<p><?php _e( 'Email has been sent!', 'wp-test-email' ); ?></p>
+							<p><?php _e( 'Email has been sent!', 'mailsender' ); ?></p>
 						</div>
 						<?php
 					}
@@ -115,7 +115,7 @@ function wp_test_email()
 					{
 						?>
 						<div class="notice notice-error is-dismissible">
-							<p><?php _e( 'Email not sent!', 'wp-test-email' ); ?></p>
+							<p><?php _e( 'Email not sent!', 'mailsender' ); ?></p>
 						</div>
 						<?php
 					}
@@ -125,17 +125,17 @@ function wp_test_email()
 		?>
 		<table class="form-table">
 			<tr valign="top">
-				<th scope="row"><?php _e( 'To', 'wp-test-email' ); ?></th>
+				<th scope="row"><?php _e( 'To', 'mailsender' ); ?></th>
 				<td>
 					<input type="email" name="mail_to" value=""/>
-					<p class="description"><i><?php _e( 'Enter "To address" here.', 'wp-test-email' ); ?></i></p>
+					<p class="description"><i><?php _e( 'Enter "To address" here.', 'mailsender' ); ?></i></p>
 				</td>
 			</tr> 
 			<tr valign="top">
-				<th scope="row"><?php _e( 'Subject', 'wp-test-email' ); ?></th>
+				<th scope="row"><?php _e( 'Subject', 'mailsender' ); ?></th>
 				<td>
 					<input type="text" name="mail_subject" value="Test Mail"/>
-					<p class="description"><i><?php _e( 'Enter mail subject here', 'wp-test-email' ); ?></i></p> 
+					<p class="description"><i><?php _e( 'Enter mail subject here', 'mailsender' ); ?></i></p> 
 				</td>
 			</tr> 			
 		</table>    
