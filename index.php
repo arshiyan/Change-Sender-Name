34b8da78e2544a3f3aa8ca5f35e12f28423340ce
javadarshiyan <dr.bloger@gmail.com>
2019-09-18 10:49:12 +0430

    update

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 /*
  * Plugin Name: Change Sender Name
@@ -9,7 +9,7 @@
  */
 
 // Don't call the file directly
-if ( !defined( 'ABSPATH' ) ) exit;
+if (!defined('ABSPATH')) exit;
 
 
 /**
@@ -17,142 +17,143 @@ if ( !defined( 'ABSPATH' ) ) exit;
  *
  * @since 1.0.0
  */
-function mailsender_load_textdomain() {
- 
- // load_plugin_textdomain( 'mailsender', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
-    load_plugin_textdomain( 'mailsender', false, basename( dirname( __FILE__ ) ) . '/languages' ); 
- 
+function csn_load_textdomain()
+{
+    load_plugin_textdomain('mailsender', false, basename(dirname(__FILE__)) . '/languages');
+}
+
+add_action('init', 'csn_load_textdomain');
+
+function csn_register()
+{
+    add_settings_section('csn_section', __('Mail Sender Options', 'mailsender'), 'csn_text', 'mailsender');
+
+    add_settings_field('csn_id', __('Mail Sender Name', 'mailsender'), 'csn_function', 'mailsender', 'csn_section');
+
+    register_setting('csn_section', 'csn_id');
+
+    add_settings_field('csn_email_id', __('Mail Sender Email', 'mailsender'), 'csn_email', 'mailsender', 'csn_section');
+
+    register_setting('csn_section', 'csn_email_id');
 
 }
 
-add_action( 'init', 'mailsender_load_textdomain' );
- 
-function mailsender_register() {
-	add_settings_section('mailsender_section', __('Mail Sender Options', 'mailsender'), 'mailsender_text', 'mailsender' );
+add_action('admin_init', 'csn_register');
 
-	add_settings_field('mailsender_id', __('Mail Sender Name','mailsender'), 'mailsender_function', 'mailsender',  'mailsender_section');
 
-	register_setting('mailsender_section', 'mailsender_id');
+function csn_function()
+{
 
-	add_settings_field('mailsender_email_id', __('Mail Sender Email', 'mailsender'), 'mailsender_email', 'mailsender',  'mailsender_section');
-
-	register_setting('mailsender_section', 'mailsender_email_id');
+    printf('<input name="csn_id" type="text" class="regular-text" value="%s" placeholder="Mail Name"/>', get_option('csn_id'));
 
 }
-add_action('admin_init', 'mailsender_register');
 
-
-
-function mailsender_function(){
-
-	printf('<input name="mailsender_id" type="text" class="regular-text" value="%s" placeholder="Mail Name"/>', get_option('mailsender_id'));
-
-}
-function mailsender_email() {
-	printf('<input name="mailsender_email_id" type="email" class="regular-text" value="%s" placeholder="no_reply@yourdomain.com"/>', get_option('mailsender_email_id'));
+function csn_email()
+{
+    printf('<input name="csn_email_id" type="email" class="regular-text" value="%s" placeholder="no_reply@yourdomain.com"/>', get_option('csn_email_id'));
 
 
 }
 
-function mailsender_text() {
+function csn_text()
+{
 
-	__('<p> You may change your WordPress Default mail sender name and email </p>','mailsender');
+    __('<p> You may change your WordPress Default mail sender name and email </p>', 'mailsender');
+
+}
+
+
+function csn_menu()
+{
+    add_menu_page(__('Mail Sender Options', 'mailsender'), __('Mail Sender', 'mailsender'), 'manage_options', 'mailsender', 'csn_output_html', 'dashicons-email');
+
+    add_submenu_page('mailsender', __('Test Mail', 'mailsender'), __('Test Mail', 'mailsender'), 'manage_options', 'csn_test_mail', 'csn_test_mail');
 
 }
 
-
-
-function mailsender_menu() {
-	add_menu_page(__('Mail Sender Options', 'mailsender'), __('Mail Sender', 'mailsender'), 'manage_options', 'mailsender', 'mailsender_output', 'dashicons-email');
-
-add_submenu_page( 'mailsender', __('Test Mail', 'mailsender'), __('Test Mail', 'mailsender'), 'manage_options', 'mailsender_test', 'mailsender_test' );
-
-}
-add_action('admin_menu', 'mailsender_menu');
+add_action('admin_menu', 'csn_menu');
 
 
 //form of email info
-function mailsender_output(){
-?>	
-	<?php settings_errors();?>
-	<form action="options.php" method="POST">
-		<?php do_settings_sections('mailsender');?>
-		<?php settings_fields('mailsender_section');?>
-		<?php submit_button();?>
-	</form>
+function csn_output_html()
+{
+    ?>
+    <?php settings_errors(); ?>
+    <form action="options.php" method="POST">
+        <?php do_settings_sections('mailsender'); ?>
+        <?php settings_fields('csn_section'); ?>
+        <?php submit_button(); ?>
+    </form>
 <?php }
 
 
 //test mail template and function
-function mailsender_test() 
-{	
-?>
-<div class="wrap">
-	<h1><?php _e( 'Test Mail', 'mailsender' ); ?></h1>
-	<form method="post">		
-		<?php
-		if(isset($_POST['mail_to']))
-		{
-			 
-			 if(wp_verify_nonce($_POST['wp_test_email_nonce_field'], 'wp_test_email_nonce_action'))
-			 {
-				if(!empty($_POST['mail_to']))
-				{
-					$to=sanitize_email($_POST['mail_to']);
-					$subject=sanitize_text_field($_POST['mail_subject']);
-					$body="hi,  This is a test email from ".get_bloginfo('name') ." that sent you";
-					$headers = array('Content-Type: text/html; charset=UTF-8'); 
-					$test_email=wp_mail( $to, $subject, $body );
-					if($test_email)
-					{
-						?>
-						<div class="notice notice-success is-dismissible">
-							<p><?php _e( 'Email has been sent!', 'mailsender' ); ?></p>
-						</div>
-						<?php
-					}
-					else
-					{
-						?>
-						<div class="notice notice-error is-dismissible">
-							<p><?php _e( 'Email not sent!', 'mailsender' ); ?></p>
-						</div>
-						<?php
-					}
-				}
-			 }
-		}
-		?>
-		<table class="form-table">
-			<tr valign="top">
-				<th scope="row"><?php _e( 'To', 'mailsender' ); ?></th>
-				<td>
-					<input type="email" name="mail_to" value=""/>
-					<p class="description"><i><?php _e( 'Enter "To address" here.', 'mailsender' ); ?></i></p>
-				</td>
-			</tr> 
-			<tr valign="top">
-				<th scope="row"><?php _e( 'Subject', 'mailsender' ); ?></th>
-				<td>
-					<input type="text" name="mail_subject" value="Test Mail"/>
-					<p class="description"><i><?php _e( 'Enter mail subject here', 'mailsender' ); ?></i></p> 
-				</td>
-			</tr> 			
-		</table>    
-		<?php wp_nonce_field( 'wp_test_email_nonce_action', 'wp_test_email_nonce_field' ); ?>
-		<?php submit_button(); ?>
-	</form>
-</div>
-<?php 
+function csn_test_mail()
+{
+    ?>
+    <div class="wrap">
+        <h1><?php _e('Test Mail', 'mailsender'); ?></h1>
+        <form method="post">
+            <?php
+            if (isset($_POST['mail_to'])) {
+
+                if (wp_verify_nonce($_POST['wp_test_email_nonce_field'], 'wp_test_email_nonce_action')) {
+                    if (!empty($_POST['mail_to'])) {
+                        $to = sanitize_email($_POST['mail_to']);
+                        $subject = sanitize_text_field($_POST['mail_subject']);
+                        $body = "hi,  This is a test email from " . get_bloginfo('name') . " that sent you";
+                        $headers = array('Content-Type: text/html; charset=UTF-8');
+                        $test_email = wp_mail($to, $subject, $body);
+                        if ($test_email) {
+                            ?>
+                            <div class="notice notice-success is-dismissible">
+                                <p><?php _e('Email has been sent!', 'mailsender'); ?></p>
+                            </div>
+                            <?php
+                        } else {
+                            ?>
+                            <div class="notice notice-error is-dismissible">
+                                <p><?php _e('Email not sent!', 'mailsender'); ?></p>
+                            </div>
+                            <?php
+                        }
+                    }
+                }
+            }
+            ?>
+            <table class="form-table">
+                <tr valign="top">
+                    <th scope="row"><?php _e('To', 'mailsender'); ?></th>
+                    <td>
+                        <input type="email" name="mail_to" value=""/>
+                        <p class="description"><i><?php _e('Enter "To address" here.', 'mailsender'); ?></i></p>
+                    </td>
+                </tr>
+                <tr valign="top">
+                    <th scope="row"><?php _e('Subject', 'mailsender'); ?></th>
+                    <td>
+                        <input type="text" name="mail_subject" value="Test Mail"/>
+                        <p class="description"><i><?php _e('Enter mail subject here', 'mailsender'); ?></i></p>
+                    </td>
+                </tr>
+            </table>
+            <?php wp_nonce_field('wp_test_email_nonce_action', 'wp_test_email_nonce_field'); ?>
+            <?php submit_button(); ?>
+        </form>
+    </div>
+    <?php
 }
 
-// Change the default wordpress@ email address
+// Change the default wordpress email address
 add_filter('wp_mail_from', '_new_mail_from');
 add_filter('wp_mail_from_name', '_new_mail_from_name');
- 
-function _new_mail_from($old) {
-	return get_option('mailsender_email_id');
+
+function _new_mail_from($old)
+{
+    return get_option('csn_email_id');
 }
-function _new_mail_from_name($old) {
-	return get_option('mailsender_id');
+
+function _new_mail_from_name($old)
+{
+    return get_option('csn_id');
 }
---
+++ readme.txt
@@ -0,0 +1,46 @@
+=== Change Mail Sender ===
+Contributors: arshiyan
+Tags:change email,change name,default email ,change Mail Sender name, change Mail Sender email, wordpress default mail sender change.
+Requires at least: 3.0.1
+Stable tag: 1.2.2
+Tested up to: 5.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Change default wordpress email
+
+== Description ==
+
+Easy way to change default email address and email name of wordpress
+
+
+== Installation ==
+
+Its easy:
+
+1.Upload plugin-name.php to the /wp-content/plugins/ directory
+2.Activate the plugin through the ‘Plugins’ menu in WordPress
+
+== Frequently Asked Questions ==
+
+= How to install this plugin? =
+
+Just follow this information
+1. Upload `plugin-name.php` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+
+= How to use this plugin? =
+
+install this plguin and then go to wp-admin panel, then check Mail Sender.
+
+== Screenshots ==
+
+1.screenshot-1 main admin view
+
+2.screenshot-2 change email and name
+
+3.screenshot-3 test email
+
+
+
+== Changelog ==
